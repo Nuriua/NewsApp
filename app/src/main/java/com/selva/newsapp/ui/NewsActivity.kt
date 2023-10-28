55ba59ff3254dfee057e658c92715fb1cd866018
Nuriya Akhmedshina <nyriya36@gmail.com>
2023-10-28 20:40:55 +0500

    check

--- app/src/main/java/com/selva/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/selva/newsapp/ui/NewsActivity.kt
@@ -3,11 +3,16 @@ package com.selva.newsapp.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.selva.newsapp.R
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?): {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
-        
+
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
