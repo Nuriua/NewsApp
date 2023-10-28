919ed23d8255b20fa15584b8ca6ab864ee84e458
Nuriya Akhmedshina <nyriya36@gmail.com>
2023-10-28 22:04:41 +0500

    check

--- app/src/main/java/com/selva/newsapp/util/Constants.kt
+++ app/src/main/java/com/selva/newsapp/util/Constants.kt
@@ -3,5 +3,6 @@ package com.selva.newsapp.util
 class Constants {
     companion object {
         const val API_KEY = "97b9cbd42fb64fbd90e67b81fd3f395b"
+        const val BASE_URL = "https://newsapi.orq"
     }
 }
