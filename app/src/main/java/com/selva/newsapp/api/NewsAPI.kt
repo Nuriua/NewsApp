be44d9e18af3d5dcd240b2bf8255674a2802a19d
Nuriya Akhmedshina <nyriya36@gmail.com>
2023-10-28 21:45:06 +0500

    check

--- app/src/main/java/com/selva/newsapp/api/NewsAPI.kt
+++ app/src/main/java/com/selva/newsapp/api/NewsAPI.kt
@@ -1,5 +1,30 @@
 package com.selva.newsapp.api
 
+import com.selva.newsapp.ui.NewsResponse
+import com.selva.newsapp.util.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface NewsAPI {
 
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode: String = "us",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode: String = "us",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
 }
---
+++ app/src/main/java/com/selva/newsapp/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.selva.newsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "97b9cbd42fb64fbd90e67b81fd3f395b"
+    }
+}
