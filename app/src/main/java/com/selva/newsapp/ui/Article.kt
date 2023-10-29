09ff8fdbceaa121ae8e8223dc094defa4a23d9f5
Nuriya Akhmedshina <nyriya36@gmail.com>
2023-10-29 08:47:05 +0500

    check

--- app/src/main/java/com/selva/newsapp/ui/Article.kt
+++ app/src/main/java/com/selva/newsapp/ui/Article.kt
@@ -1,9 +1,16 @@
 package com.selva.newsapp.ui
 
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity(
+    tableName = "articles"
+)
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
     @SerializedName("author")
     val author: String,
     @SerializedName("content")
