2b74043d0a6010263b5310d6b6ca9d97995e60f6
Nuriya Akhmedshina <nyriya36@gmail.com>
2023-10-29 21:40:20 +0500

    check

--- app/src/main/java/com/selva/newsapp/util/Resourse.kt
+++ app/src/main/java/com/selva/newsapp/util/Resourse.kt
@@ -1,4 +1,10 @@
 package com.selva.newsapp.util
 
-class Resourse {
+sealed class Resourse<T>(
+    val data:T? = null,
+    val message:String? = null
+) {
+    class Success<T>(data: T) : Resourse<T>(data)
+    class Error<T>(message: String, data: T? = null) : Resourse<T>(data, )
+    class Loasing<T>()
 }
